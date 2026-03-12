be8c8fd49d3d4b9f8f3854a7326d880f2878180c
Gabriel Mosz <96314247+AlfredoAlfredo1313@users.noreply.github.com>
2026-03-12 15:57:31 -0300

    17 Hero Anim Instance

--- Source/Varvaraa/Private/Anim/Hero/VarvaraAnimInstance.cpp
+++ Source/Varvaraa/Private/Anim/Hero/VarvaraAnimInstance.cpp
@@ -3,3 +3,28 @@
 
 #include "Anim/Hero/VarvaraAnimInstance.h"
 
+void UVarvaraAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	if (OwnerCharacter)
+	{
+		VarvaraCharacter = Cast<AVarvaraCharacter>(OwnerCharacter);
+	}
+}
+
+void UVarvaraAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+	if (bHasAcceleration)
+	{
+		if (bShouldRelax)
+		{
+			IdleElapsedTime = 0;
+			bShouldRelax = false;
+		}
+		return;
+	}
+	IdleElapsedTime += DeltaSeconds;
+	if (IdleElapsedTime >= EnterRelaxThreshold && !bShouldRelax)
+		bShouldRelax = true;
+}
--- Source/Varvaraa/Public/Anim/Hero/VarvaraAnimInstance.h
+++ Source/Varvaraa/Public/Anim/Hero/VarvaraAnimInstance.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Anim/CharacterAnimInstance.h"
+#include "Characters/VarvaraCharacter.h"
 #include "VarvaraAnimInstance.generated.h"
 
 /**
@@ -13,5 +14,21 @@ UCLASS()
 class VARVARAA_API UVarvaraAnimInstance : public UCharacterAnimInstance
 {
 	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	AVarvaraCharacter* VarvaraCharacter;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	bool bShouldRelax;
 	
+	float IdleElapsedTime;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	float EnterRelaxThreshold = 10;
 };
+		
